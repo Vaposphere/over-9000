2b7786a11f730d4b8c196214c84c44613794dc8c
Simon Hürlimann <sim@panter.ch>
2017-06-16 17:42:02 +0200

    style(convertor): use 2 space indentation

--- converter/formats/protractor.js
+++ converter/formats/protractor.js
@@ -1,42 +1,40 @@
-
-
 const locatorBy = function (locatorType) {
-	switch (locatorType) {
-		case "xpath":
-			return "xpath";
-		case "id":
-			return "id";
-		case "css selector":
-			return "css";
-		case "link text":
-			return "linkText";
-		case "class":
-			return "className";
-		case "name":
-			return "name";
-	}
+  switch (locatorType) {
+    case "xpath":
+      return "xpath";
+    case "id":
+      return "id";
+    case "css selector":
+      return "css";
+    case "link text":
+      return "linkText";
+    case "class":
+      return "className";
+    case "name":
+      return "name";
+  }
 };
 
 module.exports = {
-	writeOutput(output) {
-		const appendBefore = '    ';
+  writeOutput(output) {
+    const appendBefore = '    ';
 
-		return `
+    return `
 describe('Selenium Test Case', function() {
   it('should execute test case without errors', function() {
 ${output.map((s) => `${appendBefore}${s};`).join('\n')}
   });
 });`;
-	},
-	lineForTyp: {
-		get({url}) {
-			return `browser.get('${url}')`;
-		},
-		clickElement({locator}) {
-			return `element(by.${locatorBy(locator.type)}('${locator.value}')).click()`;
-		},
-		setElementText({locator, text}){
-			return `element(by.${locatorBy(locator.type)}('${locator.value}')).sendKeys('${text}')`;
-		}
-	},
+  },
+  lineForTyp: {
+    get({url}) {
+      return `browser.get('${url}')`;
+    },
+    clickElement({locator}) {
+      return `element(by.${locatorBy(locator.type)}('${locator.value}')).click()`;
+    },
+    setElementText({locator, text}){
+      return `element(by.${locatorBy(locator.type)}('${locator.value}')).sendKeys('${text}')`;
+    }
+  },
 };
