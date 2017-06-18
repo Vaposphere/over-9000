705db2f9b74b6f15adac3631b096399632e29d03
Simon Hürlimann <sim@panter.ch>
2017-06-18 22:18:27 +0200

    feat(converter): implement o9k cli

--- converter/index.js
+++ converter/index.js
@@ -1,7 +1,38 @@
+#! /usr/bin/env node
+
+const fs       = require('fs');
 const selenium = require('./formats/protractor.js');
 
-module.exports = function(script) {
+function transform(script) {
   const testCase = new selenium.ProtractorTestCase(script);
 
   return testCase.output();
+}
+
+class CLI {
+  help() {
+    console.log(
+`Usage: o9k [inputFile]
+`);
+  }
+
+  run() {
+    const input = process.argv[2];
+    if (!input) {
+      this.help();
+      process.exit(1);
+    }
+
+    const builderContent = fs.readFileSync(input, 'utf8');
+
+    console.log(transform(JSON.parse(builderContent)));
+  }
+}
+
+module.exports = transform;
+
+// If called directly we start the CLI
+if (require.main === module) {
+  const cli = new CLI();
+  cli.run();
 }
